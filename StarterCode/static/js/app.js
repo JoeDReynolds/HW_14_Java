106e438fcfcf614a8193ebd0e9c5aef53c3491fe
Jobie <Joe@josephs-mbp.attlocal.net>
2018-10-22 15:29:34 -0500

    few changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -16,48 +16,35 @@ tableData.forEach(function(sighting){
     });
 });
 
-// function handleSubmit() {
-//     // Prevent the page from refreshing
-//     d3.event.preventDefault();
-  
-//     // Select the input value from the form
-//     var inputValue = d3.select('#datetime').property('value')
-//     // clear the input value
-//     d3.select('#stockInput').node().value = ""
-  
-//     // Build the new table
-//     filterTable(inputValue)
-//   }
 
-//   var filteredTable = tableData.filter(sighting => sighting.datetime === inputValue);
+//Filter value based on input datetime
+submit = d3.select('#filter-btn')
 
-//   d3.select('#filter-btn').on('click', handleSubmit)
+submit.on("click", function(){
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    //Grad input
+    var input = d3.select("#datetime")
+    //Get input value
+    var inputValue = input.property("value")
 
-//Select the submit button
-var submit = d3.select("#filter-btn");
+    //Validate input
+    var dateFormat = "D/M/YYYY";
+    if (moment(inputValue, "D/M/YYYY", true).isValid() || moment(inputValue, "D/MM/YYYY", true).isValid()) {
+        console.log("Date is valid")
+        d3.select("#error").text("")
 
-submit.on("click", function() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue);
-
-  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
-  filteredData.forEach(function(sighting){
-    console.log(sighting)
-    var row = tbody.append('tr')
-    Object.entries(sighting).forEach(function([key, value]){
-        console.log(key, value);
-        var cell = tbody.append('td');
-        cell.text(value);
-    })
+        //Filter the data
+        var filterData = data.filter(function(d){
+            return d.datetime == value
+        })
+        buildTable(filterData)
+    } else {
+        console.log("Date is NOT valid")
+        d3.select("#error").text("Date is NOT valid")
+        inputvalue.property("value", "")
+        inputvalue.property("placeholder", "1/11/2011")
+        //If invalid data show all
+        buildTable(data)
+    } ; // return true
 })
-});
